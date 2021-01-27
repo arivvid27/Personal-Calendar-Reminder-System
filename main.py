c7863c104fb777c38c36a7fac5d97c4280d1146b
Videsh Arivazhagan <arivvid27@outlook.com>
2021-01-27 20:22:38 +0000

    I have fixed some errors I have encountered

--- main.py
+++ main.py
@@ -3,22 +3,22 @@ import datetime
 from time import sleep
 
 user_secure_name = input('WHAT IS YOUR REPL.IT USERNAME? > ')
-user_secure_pass = int(input('WHAT IS YOUR NUMBER PASSWORD? (just enter the password you want to create a password) > \n'))
 
 sleep(2)
 
-if user_secure_name in db and user_secure_pass in db:
+
+if user_secure_name in db:
 	print(f'Hi, {user_secure_name}!')
 	while True:
 		now = datetime.datetime.now()
 		print ("Current date and time is ")
 		print (now.strftime("%Y-%m-%d %H:%M:%S"))
-		print('Here\'s what\'s inside your locker:')
+		print('Here\'s what\'s inside your vault:')
 		keys = db.keys()
 		for keys in keys:
 			print(keys)
-			sleep(5)
-		user_locker = input('Would you like to [insert], [Remove], [Update], [Show], [quit], or ([delete] your account?) > ')
+		sleep(5)
+		user_locker = input('Would you like to [insert], [Remove], [Update], [Show], [See], [Quit], or ([Delete] your account?) > ')
 		if user_locker == 'insert':
 			user_insert_name = input('What the name of the item would you like to add? > ')
 			user_insert_value = input('What is the value of the item you would like to add? > ')
@@ -53,36 +53,27 @@ if user_secure_name in db and user_secure_pass in db:
 			print('This will now terminate...')
 			sleep(2)
 			exit()
+		elif user_locker.lower() == 'see':
+			user_see = input('What do you want to see inside? > ')
+			sleep(2)
+			print(f'Ok! here is what\'s inside {user_see}:')
+			value = db[user_see]
+			print(value)
+			sleep(5)
 		elif user_locker.lower() == 'quit':
 			print('Ok!')
 			print('Quitting...')
 			print('Bye!')
 			sleep(2)
 			exit()		
-elif user_secure_name in db and user_secure_pass not in db:
-	print('Looks like you forgot your password. Let me print your password for you...')
-	value = db[user_secure_name]
-	print(f' "{value}" ')
-	sleep(3)
-	print('Now that you have got your password back, please run the program again.')
-	sleep(1)
-	print('This program will now terminate for you.')
-	sleep(2)
-	exit()
-elif user_secure_name and user_secure_pass not in db:
+elif user_secure_name not in db:
 	print('Looks like you\'re new!')
-	print('I am setting your password right now.')
-	db[user_secure_name] = user_secure_pass
+	print('I am setting up your account right now.')
+	db[user_secure_name] = "A user"
 	sleep(2)
 	print('You now have your account!')
 	print('Please run the program again to login to your account.')
 	sleep(1)
 	print('This program will now terminate for you.')
-	sleep(2)
-	exit()
-elif user_secure_name not in db and user_secure_pass in db:
-	print('Oops. This password is either already taken, or your spelled your username wrong. Please run the program again and enter a different password for you.')
-	sleep(1)
-	print('This program will now terminate.')
 	sleep(2)
 	exit()
